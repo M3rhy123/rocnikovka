559e279306c5f5f77ea56fd5c12bd69099498c91
anoni <anonim596@email.cz>
2022-03-31 08:40:52 +0200

    update #4 - css repair, next pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import './App.css';
+let btn1 = document.getElementById('btn1')
+let btn2 = document.getElementById('btn2')
+let btn3 = document.getElementById('btn3')
+let btn4 = document.getElementById('btn4')
+let btn5 = document.getElementById('btn5')
+let btn6 = document.getElementById('btn6')
+let btn7 = document.getElementById('btn7')
+let btn8 = document.getElementById('btn8')
 let Blc = 2;
 
-function Pin_page() {
+
+const App = () => {
+    return(
+        <div></div>
+    )
+}
+
+const Pin_page = () => {
   return (
     <header className='page'>
             <a>Please Enter PIN:</a>
@@ -10,7 +25,7 @@ function Pin_page() {
   );
 }
 
-function Menu() {
+const Menu = () => {
   return(
       <div className='menu'>
         <div id='left'>
@@ -27,9 +42,10 @@ function Menu() {
         </div>
       </div>
   );
+
 }
 
-function Balance() {
+const Balance = () => {
     return(
         <div className='menu'>
             <div className='page'>
@@ -40,4 +56,56 @@ function Balance() {
 
     );
 }
-export default Balance;
+
+const Withdraw = () => {
+    return(
+        <div className='menu'>
+            <div id='left'>
+                <div className='atm_btn_short' id='balance'>1$</div>
+                <div className='atm_btn_short' id='withdraw'>2$</div>
+                <div className='atm_btn_short' id='deposit'>5$</div>
+                <div className='atm_btn_short'>10$</div>
+            </div>
+            <div className='balance'>How much do you<br/> want to Withdraw?</div>
+            <div id='right'>
+                <div className='atm_btn_short' id='pin_change'>20$</div>
+                <div className='atm_btn_short' id='payment'>50$</div>
+                <div className='atm_btn_short' id=''>Other</div>
+                <div className='atm_btn_short' id=''>Menu</div>
+            </div>
+        </div>
+    )
+}
+
+const Deposit = () => {
+    return(
+        <div className='menu'>
+            <div id='left'>
+                <div className='atm_btn_short' id='balance'>1$</div>
+                <div className='atm_btn_short' id='withdraw'>2$</div>
+                <div className='atm_btn_short' id='deposit'>5$</div>
+                <div className='atm_btn_short'>10$</div>
+            </div>
+            <div className='balance'>How much do you<br/> want to Deposit?</div>
+            <div id='right'>
+                <div className='atm_btn_short' id='pin_change'>20$</div>
+                <div className='atm_btn_short' id='payment'>50$</div>
+                <div className='atm_btn_short' id=''>Other</div>
+                <div className='atm_btn_short' id=''>Menu</div>
+            </div>
+        </div>
+    )
+}
+const Change_pin = () => {
+    return(
+        <div></div>
+    )
+}
+
+const Payment = () => {
+    return(
+        <div></div>
+    )
+}
+
+export default Menu;
